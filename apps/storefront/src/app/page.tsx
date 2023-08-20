33a027b5d6afa1c306d9d5f238bd16e8aa723639
Scott Fuoco <scottfuoco@gmail.com>
2023-08-20 07:30:59 -0700

    before removing vite and storybook files

--- apps/storefront/src/app/page.tsx
+++ apps/storefront/src/app/page.tsx
@@ -8,16 +8,14 @@ export const metadata = {
 export default function Store() {
   log("Hey! This is Home.");
   return (
-    <div className="container">
+    <div className="px-4 mx-4">
       <h1 className="title">
         Store <br />
         <span>Kitchen Sink</span>
       </h1>
       <CounterButton />
       <p className="description">
-        Built With{" "}
-        <NewTabLink href="https://turbo.build/repo">Turborepo</NewTabLink> +{" "}
-        <NewTabLink href="https://nextjs.org/">Next.js</NewTabLink>
+        Built With <NewTabLink href="https://turbo.build/repo">Turborepo</NewTabLink> + <NewTabLink href="https://nextjs.org/">Next.js</NewTabLink>
       </p>
     </div>
   );
